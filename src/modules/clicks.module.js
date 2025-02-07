fb3657ed6e6b497e017dd883fc1e192ce160911a
Ryazantsev Stanislav <ryazancev_stas94@mail.ru>
2025-02-08 03:30:45 +0700

    added modulestyle

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,100 +1,132 @@
 import { Module } from '../core/module';
 
-
 export class ClickCounter extends Module {
-  constructor() {
-    super('click-counter', 'Подсчет кликов');
-    this.leftClickCount = 0;
-    this.middleClickCount = 0;
-    this.rightClickCount = 0;
-    this.timerRunning = false;
-    this.mousedownHandler = this.mousedownHandler.bind(this);
-    this.closeModal = null;
-  }
-
-  setCloseModal(closeModal) {
-    this.closeModal = closeModal;
-  }
-
-  startClickCounter() {
-    if (this.closeModal) this.closeModal();
-    this.resetCounts();
-    this.timerRunning = true;
-
-    document.addEventListener("mousedown", this.mousedownHandler);
-
-    setTimeout(() => {
+    constructor() {
+        super('click-counter', 'Подсчет кликов');
+        this.leftClickCount = 0;
+        this.middleClickCount = 0;
+        this.rightClickCount = 0;
         this.timerRunning = false;
-        
-        const panelInfo = document.createElement('div')
-        panelInfo.style.background = 'linear-gradient(90deg, #b9deed, #efefef)'
-        panelInfo.style.display = 'flex';
-        panelInfo.style.flexDirection = 'column'; 
-        panelInfo.style.alignItems = 'center'; 
-        panelInfo.style.border = '1px solid black';  
-        panelInfo.style.padding = '10px';
-        panelInfo.style.margin = 'auto'
-
-        const panelInfoText = document.createElement('span')
-        panelInfoText.innerHTML = `Статистика кликов за 3 секунды: <br> Левая кнопка: ${this.leftClickCount}
-        <br> Средняя кнопка: ${this.middleClickCount}
-        <br> Правая кнопка: ${this.rightClickCount}`
-        panelInfoText.style.textAlign = 'center'
-        panelInfoText.style.display = 'block'
-
-        const buttonClose = document.createElement('button')
-        buttonClose.textContent = 'Закрыть';
-        buttonClose.style.display = 'inline-block';
-        buttonClose.style.backgroundColor = '#ff5555';
-        buttonClose.style.color = '#fff';
-        buttonClose.style.padding = '5px 10px';
-        buttonClose.style.border = 'none';
-        buttonClose.style.marginTop = '15px'
-        
-        buttonClose.addEventListener('mousedown', () => {
-            buttonClose.style.transform = 'scale(0.9)';
-        });
-        buttonClose.addEventListener('mouseup', () => {
-            buttonClose.style.transform = 'scale(1)';
-        });
-        buttonClose.addEventListener('click', function() {
-            panelInfo.remove()
-        })
-        
-        panelInfo.appendChild(panelInfoText)
-        document.body.append(panelInfo)
-        panelInfo.append(buttonClose)
-
-        document.removeEventListener("mousedown", this.mousedownHandler);
-    }, 3000);
-  }
-
-  mousedownHandler(event) {
-    if (!this.timerRunning) {
-      return;
+        this.mousedownHandler = this.mousedownHandler.bind(this);
+        this.closeModal = null;
     }
-    switch (event.button) {
-      case 0:
-        this.leftClickCount++;
-        break;
-      case 1:
-        this.middleClickCount++;
-        break;
-      case 2:
-        this.rightClickCount++;
-        break;
-      default:
-        console.log("Неизвестная кнопка мыши:", event.button);
+
+    setCloseModal(closeModal) {
+        this.closeModal = closeModal;
     }
-  }
 
-  resetCounts() {
-    this.leftClickCount = 0;
-    this.middleClickCount = 0;
-    this.rightClickCount = 0;
-  }
+    trigger() {
+        this.showTimerSelection();
+    }
 
-  trigger() { 
-      this.startClickCounter();
-  }
+    showTimerSelection() {
+        if (this.closeModal) this.closeModal();
+
+        const modal = document.createElement('div');
+        modal.style.position = 'fixed';
+        modal.style.top = '50%';
+        modal.style.left = '50%';
+        modal.style.transform = 'translate(-50%, -50%)';
+        modal.style.backgroundColor = '#fff';
+        modal.style.border = '1px solid #000';
+        modal.style.padding = '20px';
+        modal.style.zIndex = '1000'; 
+        modal.style.textAlign = 'center';
+
+        const timerOptions = [3, 5, 10]; // Доступные варианты таймера
+
+        const title = document.createElement('p');
+        title.textContent = 'Выберите длительность таймера (сек):';
+        modal.appendChild(title);
+
+        timerOptions.forEach(duration => {
+            const button = document.createElement('button');
+            button.textContent = `${duration} сек`;
+            button.style.margin = '5px';
+            button.addEventListener('click', () => {
+                this.startClickCounter(duration * 1000); 
+                modal.remove(); 
+            });
+            modal.appendChild(button);
+        });
+
+        document.body.appendChild(modal);
+    }
+
+    startClickCounter(duration) {
+        this.resetCounts();
+        this.timerRunning = true;
+
+        document.addEventListener("mousedown", this.mousedownHandler);
+
+        setTimeout(() => {
+            this.timerRunning = false;
+
+            const panelInfo = document.createElement('div')
+            panelInfo.style.background = 'linear-gradient(90deg,rgb(115, 198, 231),rgb(219, 113, 113))'
+            panelInfo.style.display = 'flex';
+            panelInfo.style.flexDirection = 'column';
+            panelInfo.style.alignItems = 'center';
+            panelInfo.style.border = '1px solid black';
+            panelInfo.style.padding = '10px';
+            panelInfo.style.margin = 'auto'
+
+            const panelInfoText = document.createElement('span')
+            panelInfoText.innerHTML = `Статистика кликов за ${duration / 1000} секунд: <br> Левая кнопка: ${this.leftClickCount}
+            <br> Средняя кнопка: ${this.middleClickCount}
+            <br> Правая кнопка: ${this.rightClickCount}`
+            panelInfoText.style.textAlign = 'center'
+            panelInfoText.style.display = 'block'
+
+            const buttonClose = document.createElement('button')
+            buttonClose.textContent = 'Закрыть';
+            buttonClose.style.display = 'inline-block';
+            buttonClose.style.backgroundColor = '#ff5555';
+            buttonClose.style.color = '#fff';
+            buttonClose.style.padding = '5px 10px';
+            buttonClose.style.border = 'none';
+            buttonClose.style.marginTop = '15px'
+
+            buttonClose.addEventListener('mousedown', () => {
+                buttonClose.style.transform = 'scale(0.9)';
+            });
+            buttonClose.addEventListener('mouseup', () => {
+                buttonClose.style.transform = 'scale(1)';
+            });
+            buttonClose.addEventListener('click', function() {
+                panelInfo.remove()
+            })
+
+            panelInfo.appendChild(panelInfoText)
+            document.body.append(panelInfo)
+            panelInfo.append(buttonClose)
+
+            document.removeEventListener("mousedown", this.mousedownHandler);
+        }, duration);
+    }
+
+    mousedownHandler(event) {
+        if (!this.timerRunning) {
+            return;
+        }
+        switch (event.button) {
+            case 0:
+                this.leftClickCount++;
+                break;
+            case 1:
+                this.middleClickCount++;
+                break;
+            case 2:
+                this.rightClickCount++;
+                break;
+            default:
+                console.log("Неизвестная кнопка мыши:", event.button);
+        }
+    }
+
+    resetCounts() {
+        this.leftClickCount = 0;
+        this.middleClickCount = 0;
+        this.rightClickCount = 0;
+    }
 }
