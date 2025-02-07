25205fd3196141cec106b84a00af26d368f00054
Aleksandr Vysotski <sasha.vysocki@gmail.com>
2025-02-07 15:35:56 +0300

    feature: created menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 import './styles.css'
+import { ContextMenu } from './menu.js'
 
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+
+    const contextMenu = new ContextMenu('.menu', { clickX: e.pageX, clickY: e.pageY })
+
+    contextMenu.open()
+})
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,33 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
 
 export class ContextMenu extends Menu {
 
+    constructor(selector, clickPosition) {
+        super(selector)
+        this.setPosition(clickPosition)
+    }
+
+    setPosition(clickPosition) {
+        const { clickX, clickY } = clickPosition
+
+        if ((window.innerWidth - clickX) < this.el.offsetWidth) {
+            this.el.style.left = `${clickX - this.el.offsetWidth}px`
+        } else {
+            this.el.style.left = `${clickX}px`
+        }
+
+        if ((window.innerHeight - clickY) < this.el.offsetHeight) {
+            this.el.style.top = `${clickY - this.el.offsetHeight}px`
+        } else {
+            this.el.style.top = `${clickY}px`
+        }
+    }
+
+    open() {
+        this.el.classList.add('open')
+    }
+
+    close() {
+        this.el.classList.remove('open')
+    }
 }
