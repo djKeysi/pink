965c55fd0d0893b5eff3890fc8b8e4c34ed5fbe8
Michael <mexanoid11@mail.ru>
2025-02-07 23:42:52 +0300

    done message-block mvp

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
+import { ContextMenu } from './menu'
+import { MessageModule } from './modules/message.module'
 import './styles.css'
 
+const menuBlock = document.querySelector('.menu')
+
+window.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+    menuBlock.innerHTML = new ContextMenu('.menu').open()
+
+    menuBlock.style.display = 'block'
+    menuBlock.style.left = `${event.pageX}px`
+    menuBlock.style.top = `${event.pageY}px`
+
+    menuBlock.classList.add('open')
+})
+
+menuBlock.addEventListener('click', (event) => {
+    switch (event.target.dataset.type) {
+        case 'message-module':
+            new MessageModule('message-module', 'Сообщение').trigger();
+    }
+    menuBlock.style.display = 'none'
+    menuBlock.classList.remove('open')
+})
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,19 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
+import { MessageModule } from './modules/message.module'
 
 export class ContextMenu extends Menu {
+    add() {
+        this.$messageMenuEl = new MessageModule('message-module', 'Сообщение').toHTML();
+        return this.$messageMenuEl
+    }
 
+    open() {
+        this.$menu = this.add()
+        return this.$menu
+    }
+
+    close() {
+        this.el.removeAttribute('style')
+        this.el.classList.remove('open')
+    }
 }
