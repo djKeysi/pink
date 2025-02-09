99581a5e5cf55f8c1cf27bad15f16990714a1a49
evgeniyzenchenko <zenchenko_es@outlook.com>
2025-02-09 17:23:37 +0700

    replace sound

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,5 +1,9 @@
 import { Module } from "../core/module";
 import { randomSound } from "../utils";
+import one from "../../assets/sound/1.mp3";
+import two from "../../assets/sound/2.mp3"
+import three from "../../assets/sound/3.mp3"
+import four from "../../assets/sound/4.mp3"
 
 export class SoundModule extends Module {
   constructor() {
@@ -7,13 +11,19 @@ export class SoundModule extends Module {
   }
 
   trigger() {
-    const sound =`../../assets/sound/`
+   // const sound =`../../assets/sound/`
+    // const arrSound = [
+    //   `${sound}1.mp3`,
+    //   `${sound}2.mp3`,
+    //   `${sound}3.mp3`,
+    //   `${sound}4.mp3`,
+    // ];
     const arrSound = [
-      `${sound}1.mp3`,
-      `${sound}2.mp3`,
-      `${sound}3.mp3`,
-      `${sound}4.mp3`,
-    ];
+       one,
+        two,
+        three,
+        four,
+      ];
     const audio = new Audio(arrSound[randomSound(arrSound)]);
     audio.play();
 
